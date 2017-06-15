e32f6953b8e70e394a696aebb96270eeea794ac6
Shamasis Bhattacharya <mail@shamasis.net>
2017-06-15 16:53:56 +0530

    Removed path from mocha args (again windows)

--- sanity.test.js
+++ sanity.test.js
@@ -6,23 +6,23 @@ process.env.SAILS_ENV = 'internal';
 describe('with-sails-env', function () {
 
     it('sails config to have port', function () {
-        expect(require('../../config/env/development').port).to.equal(5050);
+        expect(require('./config/env/development').port).to.equal(5050);
     });
 
     it('sails config to have environment', function () {
-        expect(require('../../config/env/development').environment).to.equal('internal');
+        expect(require('./config/env/development').environment).to.equal('internal');
     });
 
     it('sails config to have log', function () {
-        expect(require('../../config/env/development').log.level).to.equal('silent');
+        expect(require('./config/env/development').log.level).to.equal('silent');
     });
 
     it('sails config to have new variables', function () {
-        expect(require('../../config/env/development').foo.bar).to.equal('dummy');
+        expect(require('./config/env/development').foo.bar).to.equal('dummy');
     });
 
     it('sails config does not have model', function () {
-        expect(require('../../config/env/development').models).to.equal(undefined);
+        expect(require('./config/env/development').models).to.equal(undefined);
     });
 });
 
